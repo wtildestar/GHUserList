82be80b94dcdc70c9c4a4070c8ccf1950e425875
wtildestar <31348327+wtildestar@users.noreply.github.com>
2020-03-27 23:34:18 +0500

    Preparing for mvp

---
+++ GHUserList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHUserList
+//
+//  Created by wtildestar on 27/03/2020.
+//  Copyright © 2020 wtildestar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
